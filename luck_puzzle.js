664d620926f5020cbfb82596bc238ba9b253e366
ojeffcott <33331744+ojeffcott@users.noreply.github.com>
2023-03-19 17:40:11 +0000

    Create luck_puzzle.js

---
+++ luck_puzzle.js
@@ -0,0 +1,16 @@
+function chooseDoor(doorNumber) {
+  const randomOutcome = Math.floor(Math.random() * 3) + 1;
+
+  if (randomOutcome === 1) {
+    alert('Congratulations! You have completed Precinct 19.');
+    // Redirect to the victory screen or main menu
+    location.href = 'index.html';
+  } else if (randomOutcome === 2) {
+    alert('You have been sent back to the beginning. Try again!');
+    // Redirect to the homepage
+    location.href = 'index.html';
+  } else {
+    alert('You have met a terrible fate. Game over!');
+    // Redirect to the game over screen or disable game access
+  }
+}
